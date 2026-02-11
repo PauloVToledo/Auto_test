5a6e4f2568c1ce7c8c94799df0a08ab8ee666dd5
PauloVToledo <paulovicentetoledosalgado@gmail.com>
2026-02-11 17:02:30 -0300

    routes

--- backend/app/api/v1/endpoints/vehicles.py
+++ backend/app/api/v1/endpoints/vehicles.py
@@ -6,6 +6,6 @@ from app.models.vehicle import Vehicle
 router = APIRouter()
 
 
-@router.get("")
+@router.get("/", response_model=list[VehicleSchema])
 def read_vehicles(db: Session = Depends(get_db)):
     return db.query(Vehicle).all()
